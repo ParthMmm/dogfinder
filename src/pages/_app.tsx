6cf84ddf81b8af2ed6c147d205a7204ecc3ebf7c
Parth Mangrola <parthmangrola@gmail.com>
2025-02-07 10:04:08 -0800

    feat: add react query and theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,46 @@
 import { GeistSans } from "geist/font/sans";
-import { type AppType } from "next/app";
-
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import "~/styles/globals.css";
+import { ThemeProvider } from "~/components/ui/theme-provider";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import type { ReactElement, ReactNode } from "react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
+const queryClient = new QueryClient()
+
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+  theme: "string";
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const layout = getLayout(<Component {...pageProps} />);
+
   return (
-    <div className={GeistSans.className}>
-      <Component {...pageProps} />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <style jsx global>{`
+          html {
+            font-family: ${GeistSans.style.fontFamily};
+          }
+        `}</style>
+        <div className={GeistSans.className}>
+          {layout}
+        </div>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
