2f6c8a3aa8535f863084d882b57a75c59fbb38df
Parth Mangrola <parthmangrola@gmail.com>
2025-02-09 10:07:23 -0800

    feat(ui): improve dog search table ui

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,12 +2,13 @@ import { type ReactElement } from "react";
 import { AuthenticatedLayout } from "~/layout/authenticated-layout";
 import Head from "next/head";
 import { useGetDogsSearch } from "~/hooks/queries/use-get-dogs-search";
-import { Input } from "~/components/ui/input";
 
 import { Card, CardContent } from "~/components/ui/card";
 import { BreedPicker } from "~/components/breed-picker";
 
 import Image from "next/image";
+import { Button } from "~/components/ui/button";
+import { HeartIcon } from "lucide-react";
 
 export default function Page() {
   return (
@@ -18,7 +19,7 @@ export default function Page() {
       </Head>
 
       <div className="mx-auto h-full max-w-7xl">
-        <div className="mx-auto space-y-6 py-4 md:space-y-12 md:py-10">
+        <div className="mx-auto space-y-6 px-4 py-4 md:space-y-12 md:py-10">
           <div className="flex flex-row items-center justify-between align-middle">
             <div className="text-left">
               <div className="text-3xl font-bold tracking-tight">
@@ -26,7 +27,7 @@ export default function Page() {
               </div>
             </div>
           </div>
-          <DogTable />
+          <DogSearch />
         </div>
       </div>
     </>
@@ -37,46 +38,93 @@ Page.getLayout = function getLayout(dashboardPage: ReactElement) {
   return <AuthenticatedLayout>{dashboardPage}</AuthenticatedLayout>;
 };
 
+function DogSearch() {
+  return (
+    <div>
+      <div className="flex flex-row items-center gap-2 pb-2">
+        <BreedPicker />
+      </div>
+      <DogTable />
+    </div>
+  );
+}
+
 function DogTable() {
   const { data, isPending, isError } = useGetDogsSearch();
 
   if (!data) {
     return null;
   }
-
-  if (isPending) {
-    return <div>loading...</div>;
-  }
-
   if (isError) {
     return <div>error</div>;
   }
 
+  if (isPending) {
+    return (
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <Card key={index} className="w-full md:min-w-[292px]">
+            <CardContent className="p-0">
+              <div className="aspect-[3/4] w-full animate-pulse rounded-t-md bg-stone-500 object-cover group-hover:opacity-75 sm:aspect-auto sm:h-96 md:w-[292px]" />
+
+              <div className="p-4">
+                <div className="h-6 w-full animate-pulse rounded-md bg-stone-500" />
+                <div className="mt-1 h-4 w-full animate-pulse rounded-md bg-stone-500" />
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <div className="flex flex-row items-center gap-2">
-        <Input className="max-w-[200px]" />
-        <BreedPicker />
-      </div>
+    <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      {data.map((dog) => {
+        return (
+          <Card key={dog.id} className="w-full md:min-w-[292px]">
+            <CardContent className="p-0">
+              <Image
+                src={dog.img}
+                width={292}
+                height={389}
+                className="aspect-[3/4] w-full rounded-t-md bg-gray-200 object-cover sm:aspect-auto sm:h-96 md:w-[292px]"
+                alt="picture of a dog"
+              />
 
-      <div className="grid grid-cols-4">
-        {data.map((dog) => {
-          return (
-            <Card key={dog.id}>
-              <CardContent>
-                <Image
-                  src={dog.img}
-                  width={500}
-                  height={500}
-                  alt="Picture of the author"
-                />
-
-                {dog.name}
-              </CardContent>
-            </Card>
-          );
-        })}
-      </div>
+              <div className="flex flex-row justify-between p-4">
+                <div>
+                  <div className="flex flex-col gap-0.5">
+                    <h3 className="text-xl font-medium leading-4">
+                      {dog.name}
+                    </h3>
+                    <p className="text-lg text-neutral-700 dark:text-neutral-400">
+                      {dog.breed}
+                    </p>
+                  </div>
+                  <div className="flex flex-row items-center gap-2 pt-2">
+                    {dog.age === 0 ? (
+                      <p className="text-sm">Puppy</p>
+                    ) : (
+                      <p className="text-sm">{dog.age} years old</p>
+                    )}
+                  </div>
+                  <p className="text-sm tabular-nums">{dog.zip_code}</p>
+                </div>{" "}
+                <div>
+                  <Button
+                    variant="none"
+                    size="icon"
+                    className="hover:text-red-500"
+                  >
+                    <HeartIcon className="h-4 w-4" />
+                  </Button>
+                </div>
+              </div>
+            </CardContent>
+          </Card>
+        );
+      })}
     </div>
   );
 }
